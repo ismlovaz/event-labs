c42e8ef953b8e7e8edae766a13cca7db07d53efc
ismlovaz <azizismailov872872@gmail.com>
2026-01-06 20:14:34 +0900

    fix: switch runtime to nodejs to avoid size limit

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,7 +1,7 @@
 import { httpHandler } from "@/server"
 import { NextRequest } from "next/server"
 
-export const runtime = "edge"
+export const runtime = "nodejs"
 
 export const GET = (req: NextRequest) => httpHandler(req, {} as any)
 export const POST = (req: NextRequest) => httpHandler(req, {} as any)
